027caccfb0f4e246c3b0060f2eebc169acdc69c1
autoapplyai <autoapplyai22@gmail.com>
2025-07-30 17:56:21 -0500

    Update generate_application.py

--- generate_application.py
+++ generate_application.py
@@ -10,14 +10,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def setup_driver():
     opts = webdriver.ChromeOptions()
-    opts.add_argument("--headless=new")
-    opts.add_argument("--disable-gpu")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
-    opts.add_argument("--remote-debugging-port=9222")
-    opts.add_argument("--single-process")
-    opts.add_argument("--disable-extensions")
-    opts.binary_location = "/usr/bin/chromium-browser"
+    opts.add_argument("--disable-gpu")
     svc = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=svc, options=opts)
 
